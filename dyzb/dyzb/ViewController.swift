c5ab7eae5650c5d40c07bae8d6aafd0611be6f94
nizainaer <1025067537@qq.com>
2019-03-04 22:25:02 -0800

    提交图片

--- dyzb/dyzb/ViewController.swift
+++ dyzb/dyzb/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("bbbb")
+        print("打印aaaa")
     }
 
     override func didReceiveMemoryWarning() {
